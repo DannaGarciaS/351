b64ff4420869cc412fd5da13488036236689d3b9
DannaGarciaS <danna.paolaga007@gmail.com>
2023-09-06 17:47:44 -0600

    agrege holamundo

---
+++ inicial/Unidad1/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+package holamundo;
+
+/**
+ *
+ * @author danna
+ */
+public class HolaMundo {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
